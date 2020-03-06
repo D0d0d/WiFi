649d1519c10e55325856839f1a660f7e5628fd08
Dod0d <pixonys@mail.ru>
2020-03-06 23:54:02 +0300

    Готовое

--- WiFi/Form1.Designer.cs
+++ WiFi/Form1.Designer.cs
@@ -29,12 +29,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listNet = new System.Windows.Forms.ListView();
+            this.SSID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Signal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Crypt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listNet
+            // 
+            this.listNet.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SSID,
+            this.Signal,
+            this.Type,
+            this.Crypt});
+            this.listNet.HideSelection = false;
+            this.listNet.LargeImageList = this.imageList1;
+            this.listNet.Location = new System.Drawing.Point(12, 12);
+            this.listNet.Name = "listNet";
+            this.listNet.Size = new System.Drawing.Size(666, 407);
+            this.listNet.SmallImageList = this.imageList1;
+            this.listNet.TabIndex = 0;
+            this.listNet.UseCompatibleStateImageBehavior = false;
+            this.listNet.View = System.Windows.Forms.View.Details;
+            this.listNet.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // SSID
+            // 
+            this.SSID.Text = "SSID";
+            this.SSID.Width = 100;
+            // 
+            // Signal
+            // 
+            this.Signal.Text = "Качество";
+            this.Signal.Width = 100;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Тип безопасности";
+            this.Type.Width = 100;
+            // 
+            // Crypt
+            // 
+            this.Crypt.Text = "Тип шифрования";
+            this.Crypt.Width = 100;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "WIFI.png");
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(684, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(104, 33);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Обновить";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listNet);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listNet;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ColumnHeader SSID;
+        private System.Windows.Forms.ColumnHeader Signal;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Crypt;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
--- WiFi/Form1.cs
+++ WiFi/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using NativeWifi;
+
+
+
 namespace WiFi
 {
     public partial class Form1 : Form
@@ -15,6 +19,31 @@ namespace WiFi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            WlanClient client = new WlanClient();
+            foreach (WlanClient.WlanInterface wlanInterface in client.Interfaces )
+            {
+                Wlan.WlanAvailableNetwork[] wlanBssEntries = wlanInterface.GetAvailableNetworkList(0);
+                listNet.Items.Clear();
+                foreach (Wlan.WlanAvailableNetwork network in wlanBssEntries)
+                {
+                    ListViewItem listItemWifi = new ListViewItem();
+                    listItemWifi.Text = System.Text.ASCIIEncoding.ASCII.GetString(network.dot11Ssid.SSID).Trim((char)0);
+                    listItemWifi.SubItems.Add(network.wlanSignalQuality.ToString() + "%");
+                    listItemWifi.SubItems.Add(network.dot11DefaultAuthAlgorithm.ToString().Trim((char)0));
+                    listItemWifi.SubItems.Add(network.dot11DefaultCipherAlgorithm.ToString().Trim((char)0));
+                    listItemWifi.ImageIndex = 0;
+                    listNet.Items.Add(listItemWifi);
+                }
+            }
         }
     }
 }
